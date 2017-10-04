bbb06ca4009d9682b592aff1eba1c78a854df7cc
iaw26509397 <iaw26509397@h14.informatica.escoladeltreball.org>
2017-10-04 17:15:59 +0200

    LinkedList introduced

--- src/org/escoladeltreball/collectionsdemo1/Main.java
+++ src/org/escoladeltreball/collectionsdemo1/Main.java
@@ -4,8 +4,11 @@
 package org.escoladeltreball.collectionsdemo1;
 
 
-import java.util.*;
-import java.util.ArrayList;
+//import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * @author iaw26509397
  *
@@ -16,7 +19,7 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		List<String> list = new ArrayList();
+		List<String> list = new LinkedList<>();
 		
 		list.add("Alpha");
 		list.add("Charlie");
