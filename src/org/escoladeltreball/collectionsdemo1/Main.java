8eed880bcb4d4d5f2f1321c5b2e7d2e60d967482
iaw26509397 <iaw26509397@h14.informatica.escoladeltreball.org>
2017-10-04 17:02:52 +0200

    commit list

--- src/org/escoladeltreball/collectionsdemo1/Main.java
+++ src/org/escoladeltreball/collectionsdemo1/Main.java
@@ -3,6 +3,9 @@
  */
 package org.escoladeltreball.collectionsdemo1;
 
+
+import java.util.*;
+import java.util.ArrayList;
 /**
  * @author iaw26509397
  *
@@ -13,7 +16,24 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		List<String> list = new ArrayList();
+		
+		list.add("Alpha");
+		list.add("Charlie");
+		list.add("Bravo");
+		list.add("Echo");
+		list.add("Delta");
+		list.add("Zulu");
+		
+		list.remove("Charlie");
+		
+		Collections.sort(list);
+		
+		for (String string: list) {
+			System.out.println(string);
+		}
+		
+		
 
 	}
 
