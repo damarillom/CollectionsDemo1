c4e420abba2f17811c7dfe4868bb30e0bb2a9c91
iaw26509397 <iaw26509397@h14.informatica.escoladeltreball.org>
2017-10-04 17:01:53 +0200

    commit

---
+++ src/org/escoladeltreball/collectionsdemo1/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.escoladeltreball.collectionsdemo1;
+
+/**
+ * @author iaw26509397
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
